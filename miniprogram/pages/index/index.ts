a5cd0a6e69f50c24588c1c52e3f678a79781ebcd
梅志威 <meizhiwei@tezign.com>
2019-11-15 14:20:37 +0800

    修改编辑器规范

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -6,69 +6,69 @@ const imageUrl = 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_1000
 const app = getApp<IMyApp>()
 
 Page({
-  data: {
-    motto: new Date().getMonth() + '-' + new Date().getDate() + '-' +new Date().getDay(),
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    coverList: [
-      imageUrl,
-      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572416287354&di=dbd620a6449737e40a40db23257bc81f&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fw%3D580%2Fsign%3D4d5e01bdba389b5038ffe05ab534e5f1%2F8cca9b8fa0ec08fa2ab208045aee3d6d54fbda28.jpg',
-      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572418396636&di=3b0ec109ce39249a5a4bb7aa1c434317&imgtype=0&src=http%3A%2F%2Fimg.zhichiwangluo.com%2Fzcimgdir%2Falbum%2Ffile_5aae72afb667a.jpg',
-    ],
-    movieList: [
-      {
-        src: imageUrl,
-        score: 4.2,
-        name: '血战湘江',
-        labels: ['战争', '爱情']
-      },
-      {
-        src: imageUrl,
-        score: 4.2,
-        name: '血战湘江',
-        labels: ['战争', '爱情']
-      }
-    ]
-  },
-  //事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  clickMe() {
-    this.setData!({motto: '哈哈哈哈'})
-  },
-  onLoad() {
-    if (app.globalData.userInfo) {
-      this.setData!({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true,
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = (res) => {
-        this.setData!({
-          userInfo: res,
-          hasUserInfo: true
+    data: {
+        motto: new Date().getMonth() + '-' + new Date().getDate() + '-' + new Date().getDay(),
+        userInfo: {},
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo'),
+        coverList: [
+            imageUrl,
+            'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572416287354&di=dbd620a6449737e40a40db23257bc81f&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fforum%2Fw%3D580%2Fsign%3D4d5e01bdba389b5038ffe05ab534e5f1%2F8cca9b8fa0ec08fa2ab208045aee3d6d54fbda28.jpg',
+            'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572418396636&di=3b0ec109ce39249a5a4bb7aa1c434317&imgtype=0&src=http%3A%2F%2Fimg.zhichiwangluo.com%2Fzcimgdir%2Falbum%2Ffile_5aae72afb667a.jpg',
+        ],
+        movieList: [
+            {
+                src: imageUrl,
+                score: 4.2,
+                name: '血战湘江',
+                labels: ['战争', '爱情']
+            },
+            {
+                src: imageUrl,
+                score: 4.2,
+                name: '血战湘江',
+                labels: ['战争', '爱情']
+            }
+        ]
+    },
+    //事件处理函数
+    bindViewTap() {
+        wx.navigateTo({
+            url: '../logs/logs'
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData!({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+    },
+    clickMe() {
+        this.setData!({ motto: '哈哈哈哈' })
+    },
+    onLoad() {
+        if (app.globalData.userInfo) {
+            this.setData!({
+                userInfo: app.globalData.userInfo,
+                hasUserInfo: true,
+            })
+        } else if (this.data.canIUse) {
+            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+            // 所以此处加入 callback 以防止这种情况
+            app.userInfoReadyCallback = (res) => {
+                this.setData!({
+                    userInfo: res,
+                    hasUserInfo: true
+                })
+            }
+        } else {
+            // 在没有 open-type=getUserInfo 版本的兼容处理
+            wx.getUserInfo({
+                success: res => {
+                    app.globalData.userInfo = res.userInfo
+                    this.setData!({
+                        userInfo: res.userInfo,
+                        hasUserInfo: true
+                    })
+                }
+            })
         }
-      })
+    },
+    onClick(e: any) {
+        console.log(e);
     }
-  },
-  onClick(e: any) {
-    console.log(e);
-  }
 })
